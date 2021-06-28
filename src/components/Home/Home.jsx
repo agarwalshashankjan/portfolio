792cf23116066d4b56323db5037241b0df6c01a6
agarwalshashankjan <agarwalshashank.jan>
2021-06-28 15:17:54 +0530

    Completed Home Page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
+import homeAnime from "./../../assets/home_anime.gif";
 
-const Home = () => {
-  return (
-    <div>
-      <h1>I'm Home page!</h1>
-    </div>
-  );
-};
+class Home extends Component {
+  render() {
+    return (
+      <div className="home-container">
+        <div className="header-text">
+          <h1>Welcome to my Portfolio!</h1>
+          <p>This is Shashank, a full stack dev and a constant learner!</p>
+        </div>
+        <div className="head-btns">
+          <Link to="/about" className="btn btn-white">
+            <p className="btn-text">know more about me</p>
+          </Link>
+          <Link to="/contact" className="btn btn-transparent">
+            <p className="btn-text">Connect with me</p>
+          </Link>
+        </div>
+        <div className="splash-image">
+          <img src={homeAnime} alt="animation" className="home-anime" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;

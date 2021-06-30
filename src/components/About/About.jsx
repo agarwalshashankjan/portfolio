9ce75e6103decf23443df751298f0a8068042614
agarwalshashankjan <agarwalshashank.jan>
2021-06-30 16:01:05 +0530

    Footer component complete

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 import "./About.css";
 import aboutVector from "./../../assets/about_vector.png";
 
@@ -8,9 +9,10 @@ const About = () => {
     <div className="section-container">
       <Header
         heading="About Me."
-        details="The Gluttony sin | Artist | Activist | Full Stack dev | COnstant Leaner"
+        details="The Gluttony sin | Artist | Activist | Full Stack dev | Constant Leaner"
       />
 
+      <Footer phrase="Check out my " link="projects!" toAddress="/projects" />
       {/* Vector Frame */}
       <div className="vector-frame">
         <img src={aboutVector} alt="about" className="about-vector" />

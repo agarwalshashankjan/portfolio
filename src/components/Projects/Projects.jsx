6fd1ccb170de0ea6d8933f052ddaa30991394ca9
agarwalshashankjan <agarwalshashank.jan>
2021-07-03 18:47:53 +0530

    Completed projects page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import "./Projects.css";
+import Header from "./../Header/Header";
+import Footer from "./../Footer/Footer";
+import { projectsData } from "./../../assets/projectsData";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   return (
     <div className="section-container">
-      <h1>I'm Projects page!</h1>
+      <Header
+        heading="My Projects."
+        details="Here are a few cool things I've worked!!!"
+      />
+
+      <div className="projects-card-container">
+        {projectsData.map(
+          ({ projectName, projectDescription, imageUrl, projectUrl }) => {
+            return (
+              <ProjectCard
+                projectName={projectName}
+                imageUrl={imageUrl}
+                projectDescription={projectDescription}
+                projectUrl={projectUrl}
+              />
+            );
+          }
+        )}
+      </div>
+
+      <Footer phrase="Check out " link="my skills!" toAddress="/skills" />
     </div>
   );
 };

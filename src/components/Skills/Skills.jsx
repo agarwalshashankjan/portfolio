98d7bed67162419351b81209fd221b6efc3472ba
agarwalshashankjan <agarwalshashank.jan>
2021-07-03 14:34:47 +0530

    skills page completed

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Header from "./../Header/Header";
+import Footer from "./../Footer/Footer";
+import { skillList } from "./../../assets/skillsData";
 import "./Skills.css";
+import skillsVector from "./../../assets/skills_vector.png";
+import SkillCard from "./SkillCard";
 
 const Skills = () => {
   return (
     <div className="section-container">
-      <h1>I'm Skills page!</h1>
+      <Header
+        heading="My skills."
+        details="Passionate about new technologies, I keep expploring stuff! Here is the tech stack I've  worked with."
+      />
+      <div className="skills-card-container">
+        {skillList.map(({ skillName, skillUrl }) => (
+          <SkillCard skillName={skillName} skillUrl={skillUrl} />
+        ))}
+      </div>
+      <Footer phrase="Get in " link="touch." toAddress="/contact" />
+      <div className="skills-vector-frame">
+        <img src={skillsVector} alt="Skills" className="skills-vector" />
+      </div>
     </div>
   );
 };
